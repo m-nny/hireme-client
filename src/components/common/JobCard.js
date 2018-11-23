046403331a1a75e54d2af3ddb5869d99e54780ee
yesass <assem.yeskabyl@nu.edu.kz>
2018-11-23 16:25:18 +0600

    CompanyProfile,CreateOffer, JobCard

--- src/components/common/JobCard.js
+++ src/components/common/JobCard.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import '../../styles/JobCard.sass';
+import {Column, Row} from 'simple-flexbox';
 
-const JobCard = ({job: {role, description_of_responsibilities}}) => (
+const JobCard = ({job: {company, position, responsibilities, qualifications, location}}) => (
 	<div className='job-card'>
-		<div className='job-role'>{role}</div>
-		<div className='job-responsibilities'>{description_of_responsibilities}</div>
+		<Column>
+			<Row horizontal="center"><div className='company-name'>{company.name}</div></Row>
+			<Row horizontal="center">
+				<Column className="Description">
+					<div>Position:</div>
+					<div>Responsibilities:</div>
+					<div>Minimum Qualifications:</div>
+					<div>Location:</div>
+				</Column>
+				<Column className="fields">
+					<div className='position'>{position}</div>
+					<div className='responsibilities'>{responsibilities}</div>
+					<div className='qualifications'>{qualifications}</div>
+					<div className='location'>{location}</div>
+				</Column>
+			</Row>
+			<Row horizontal="center">
+				<button>Apply</button>
+			</Row>
+		</Column>
 	</div>
 );
 
