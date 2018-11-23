33adb32096736967422cfdf707a2fa9a1cfaea99
yesass <assem.yeskabyl@nu.edu.kz>
2018-11-23 11:54:50 +0600

    Posts2(css)

--- src/components/common/PostCard.js
+++ src/components/common/PostCard.js
@@ -6,7 +6,7 @@ const PostCard = ({post: {id, company, author, text, photo_link, jobOffers}}) =>
 	<div className="post-card">
 		<div className="author-title">{author.name}</div>
 		<div className="post-content">{text}</div>
-		<img src="photo-link"></img>
+		<img src="https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&h=350"></img>
 		<div className="job-section">
 			{jobOffers.map(job => <JobCard job={job} key={job.id}/>)}
 		</div>
